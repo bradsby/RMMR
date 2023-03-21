d37b7e5733d6022e45480aea62a27b6fc7b933ea
bradsby <82857316+bradsby@users.noreply.github.com>
2023-03-21 11:13:18 -0500

    Add page icon

--- rmmr.py
+++ rmmr.py
@@ -16,7 +16,7 @@ from sklearn.cluster import KMeans
 
 st.set_page_config(
     page_title="RMMR",
-    page_icon='❓'
+    page_icon='❓',
     layout="wide",
     initial_sidebar_state="expanded",
 )
